3991f3be6b98817f85a0438cfb44d2634cf33706
yasminbrombach <yasminbrombach88@gmail.com>
2026-08-11 11:20:08 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "pedersen-realestate.com",
+      },
+    ],
+  },
+};
 
+export default nextConfig;
